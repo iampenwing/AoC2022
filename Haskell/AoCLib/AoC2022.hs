377b72fe5d8747d23b5ae0913a35addb7c802b63
penwing <alex@penwing.me.uk>
2022-12-03 10:07:51 +0000

    Day 3 Haskell Code with additions to the Library

--- Haskell/AoCLib/AoC2022.hs
+++ Haskell/AoCLib/AoC2022.hs
@@ -1,7 +1,10 @@
 module AoCLib.AoC2022
   (
     myReadInt,
-    countLists
+    countLists,
+    commonElement,
+    splitListInHalf,
+    findDuplicate
   ) where
 
 myReadInt :: [Char] -> Int
@@ -18,4 +21,31 @@ iCountLists :: Int -> [String] -> (Int, [String])
 iCountLists count [] = (count, [])
 iCountLists count ("":xs) = (count, xs)
 iCountLists count (x:xs) = iCountLists (count + (myReadInt x)) xs
+
+commonElement :: [String] -> Char
+commonElement ((x:xs):ys) =
+  if (commonElementHelper x ys)
+  then x
+  else commonElement (xs:ys)
+
+commonElementHelper :: Char -> [String] -> Bool
+commonElementHelper _ [] = False
+commonElementHelper c ([]:ys) = False
+commonElementHelper c ((x:xs):[]) =
+  if (c == x)
+  then True
+  else commonElementHelper c (xs:[])
+commonElementHelper c ((x:xs):ys) =
+  if (c == x)
+  then commonElementHelper c ys
+  else commonElementHelper c (xs:ys)
+
+splitListInHalf :: String -> (String, String)
+splitListInHalf ls = splitAt ((length ls) `div` 2) ls
+
+findDuplicate :: (String, String) -> Char
+findDuplicate ((x:xs), y) =
+  if (elem x y)
+     then x
+     else (findDuplicate (xs, y))
 
---
+++ Haskell/Day 03-1.hs
@@ -0,0 +1,26 @@
+-- AdventOfCode 2022 Day 1, Puzzle 1
+-- Calorie Counting
+-- https://adventofcode.com/2022/day/1
+-- Counts when depth increases
+
+-- Alex Lambert (penwing)
+-- aoc@penwing.me.uk
+
+-- import File
+import Data.String
+import System.Environment
+import AoCLib.AoC2022 as AoC
+
+day3Task1Score :: Char -> Int
+day3Task1Score c =
+  let i = (fromEnum c)
+  in if (i < 91)
+     then (i - 38)
+     else (i - 96)
+            
+main :: IO()
+main = do
+  [fileInput] <- getArgs
+  fileContents <- readFile fileInput
+  let answer = (foldr (+) 0 (map (day3Task1Score) (map (findDuplicate) (map (splitListInHalf) (lines fileContents)))))
+         in putStrLn (show answer)
