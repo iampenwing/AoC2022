706da66a870e4cd01e15cd28dd6369efc577a038
iampenwing <34182537+iampenwing@users.noreply.github.com>
2022-12-07 18:42:47 +0000

    Update Day 07-1.hs
    Maybe I now have data structures...? Working on Parsing Commands...

--- Haskell/Day 07-1.hs
+++ Haskell/Day 07-1.hs
@@ -7,22 +7,61 @@ import Data.List.Split
 import System.Environment
 import AoCLib.AoC2022 as AoC
 
+import Data.List.Split
+
 data MyDirectory = MyFile { fileName::String 
                           , fileSize::Int }
                  | MyDirectory { dirName::String 
                                , dirSize::Int 
-                               , dirContents::[MyDirectory] } deriving (Show)
+                               , dirContents::[MyDirectory]
+                               , fileContents::[MyDirectory] } deriving (Show)
 
-testData = MyDirectory {dirName="/", dirSize=0, dirContents=[(MyFile {fileName="a.txt", fileSize=200}), (MyDirectory {dirName="dirA", dirSize=0, dirContents=[(MyFile {fileName="b.txt", fileSize=200}),(MyFile {fileName="c.txt", fileSize=200})]})]}
+data Command = CD String
+             | Down
+             | Listing [MyDirectory] deriving (Show)
+             
+
+rootDir = MyDirectory {dirName="/", dirSize=0, dirContents=[], fileContents=[]}
+
+
+addToDirectory :: MyDirectory -> MyDirectory -> MyDirectory
+addToDirectory (MyDirectory newDirName newDirSize newDirContents newFileContents) (MyDirectory currentDirName currentDirSize currentDirContents currentFileContents) = (MyDirectory currentDirName currentDirSize ((MyDirectory newDirName newDirSize newDirContents newFileContents):currentDirContents) currentFileContents)
+addToDirectory (MyFile newFileName newFileSize) (MyDirectory currentDirName currentDirSize currentDirContents currentFileContents) = (MyDirectory currentDirName currentDirSize currentDirContents ((MyFile newFileName newFileSize):currentFileContents))
+
+getDirectory :: String -> MyDirectory -> MyDirectory
+getDirectory dirName (MyDirectory _ _ contents _) = getDirectoryHelper dirName contents
+
+getDirectoryHelper :: String -> [MyDirectory] -> MyDirectory
+getDirectoryHelper dirName ((MyDirectory topDirName topDirSize topDirContents topFileContents):rest) 
+  | dirName == topDirName = (MyDirectory topDirName topDirSize topDirContents topFileContents)
+  | otherwise = getDirectoryHelper dirName rest
+getDirectoryHelper dirName ((MyFile _ _):rest) = getDirectoryHelper dirName rest
 
 updateSizes :: MyDirectory -> MyDirectory
 updateSizes (MyFile n s) = (MyFile n s)
-updateSizes (MyDirectory n s c) = let updatedContents = map updateSizes c
-  in (MyDirectory n (foldr (+) 0 (map getSize updatedContents)) updatedContents)
+updateSizes (MyDirectory n s c f) = let updatedContents = map updateSizes c
+  in (MyDirectory n ((foldr (+) 0 (map getSize updatedContents)) + (foldr (+) 0 (map getSize f))) updatedContents f)
 
 getSize :: MyDirectory -> Int
 getSize (MyFile _ s) = s
-getSize (MyDirectory _ s _) = s
+getSize (MyDirectory _ s _ _) = s
+
+makeCommands :: [String] -> [Command]
+makeCommands [] = []
+makeCommands ((c:(" ":cs)):rest)
+  | c == '$' && cs == "cd .." = (Down:(makeCommands rest))
+  | c == '$' && (take 2 cs) == "cd" = ((CD (drop 3 cs)):(makeCommands rest))
+  | c == '$' && cs == "ls" = ((Listing getListing rest):(makeCommands rest))
+  | otherwise = makeCommands rest
+
+getListing :: [String] -> [MyDirectory]
+getListing [] = []
+getListing (x:xs) 
+  | (head x) == '$' = []
+  | (splitX!!0) == "dir" = (MyDirectory (splitX!!1) 0 [] []):(getListing xs)
+  | otherwise = (MyFile splitX!!1 (read (splitX!!0))::Int):(getListing xs)
+    where splitX = splitOn " " x
+
 
 main :: IO()
 main = do
